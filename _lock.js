5c4b0b0579477a8da773f6d840b06589cc6ee0e4
Felix <fschwieb@protonmail.com>
2026-02-08 18:17:03 -0500

    Fix syntax error in cookie check condition

--- _lock.js
+++ _lock.js
@@ -1,5 +1,8 @@
+console.log("Lock script loaded");
+
 document.addEventListener("DOMContentLoaded", () => {
-  if (!document.cookie.includes("spectrum_access=true") { // Not logged in or just iPad kid, so redirect
+  if (!document.cookie.includes("spectrum_access=true")) { // Not logged in or just iPad kid, so redirect
+    console.log("Not logged in");
     window.location.href = "index.html";
   }
 });
